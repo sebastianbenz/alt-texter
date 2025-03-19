f01c7304f533c85fc0a5dddb02256f60a6abd0b6
Sebastian Benz <sbenz@google.com>
2025-03-19 22:13:17 +0100

    add translater

--- popup.js
+++ popup.js
@@ -1,13 +1,40 @@
 const altTextInput = document.getElementById("altText");
 const loading = document.getElementById("loading");
+const lang = document.getElementById("lang");
+let text = "";
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === "alt-text") {
-    altTextInput.value = request.text;
-    loading.setAttribute('hidden', true);
-    altTextInput.removeAttribute('hidden');
-  }
+lang.addEventListener("change", async function () {
+  altTextInput.setAttribute("hidden", true);
+  loading.removeAttribute("hidden");
+  text = await translate(text);
+  showAltText();
 });
+
+async function translate(string) {
+  const translator = await self.ai.translator.create({
+    sourceLanguage: "en",
+    targetLanguage: lang.value,
+  });
+  return translator.translate(string);
+}
+
+async function showAltText() {
+  altTextInput.value = text;
+  loading.setAttribute("hidden", true);
+  altTextInput.removeAttribute("hidden");
+}
+
+chrome.runtime.onMessage.addListener(
+  async function (request, sender, sendResponse) {
+    if (request.action === "alt-text") {
+      text = request.text;
+      if (lang.value != "en") {
+        text = await translate(text);
+      }
+      showAltText();
+    }
+  }
+);
 
 document.getElementById("copyClose").addEventListener("click", () => {
   const altText = altTextInput.value;
